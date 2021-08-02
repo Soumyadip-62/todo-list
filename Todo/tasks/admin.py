cf58ca02b4dac511a6512bdef39f35a6c8c00df6
Soumyadip-62 <panditsoumya35@gmail.com>
2021-08-02 10:58:11 +0530

    second commit

--- Todo/tasks/admin.py
+++ Todo/tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tasks,Goals
 
 # Register your models here.
+
+admin.site.register(Tasks)
+admin.site.register(Goals)
--- Todo/tasks/models.py
+++ Todo/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    tasks_name = models.CharField(max_length=30)
+    tasks_details = models.CharField(max_length=90)
+    task_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.tasks_name
+    
+class Goals(models.Model):
+    g_name = models.CharField(max_length=30)
+    g_details = models.CharField(max_length=90)
+    g_date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
+    def __str__(self):
+        return self.g_name
+    
